1599c0c13059a94c28ae946b3d21da74b371e282
Kunal <kunal22666@gmail.com>
2023-05-30 17:46:57 +0530

    added cover page

---
+++ index.js
@@ -0,0 +1,9 @@
+AOS.init()
+let typed = new Typed('.typeclass', {
+    strings: ["I'm Kunal.", "I'm a Freelancer.","I'm a Coder.","I'm a Developer."],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true,
+    startDelay:50,
+    loopCount: Infinity
+  });
